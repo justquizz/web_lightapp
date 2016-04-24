80f4084f31e6885f75595545f110740c35b05e5f
eugen <info@lightapp.ru>
2016-04-24 23:19:52 +0300

    create rules

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,14 @@ function createTest(){
 	alert('Hello!');
 
 }
+
+function hideRules(){
+	//alert('hideRules!');
+	var rules = document.getElementById("rules");
+	
+	if (rules.style.display == "none")
+		{rules.style.display = "block"}
+	else
+		{rules.style.display = "none"}
+
+}
